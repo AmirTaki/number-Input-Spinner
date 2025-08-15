04613bc2c682317e5246fa0d8fb5c2dac2752b39
amir <amirtaki623@gmail.com>
2025-08-15 23:58:56 +0330

    part 16

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 let numbers = document.getElementById("box")
 
 for(let i = 0; i< 100 ; i++){
-    
+    let span = document.createElement("span")  
+    span.textContent = i;
+    numbers.appendChild(span)
 }
