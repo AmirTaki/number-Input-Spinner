a60ee33e1ff499e5eec807f9576e1e88a98ef488
amir <amirtaki623@gmail.com>
2025-08-16 00:30:38 +0330

    complete project part 3

--- src/main.js
+++ src/main.js
@@ -1,25 +1,5 @@
 let numbers = document.getElementById("box")
 
-// for(let i = 0; i< 100 ; i++){
-//     let span = document.createElement("span")  
-//     span.textContent = i;
-//     numbers.appendChild(span)
-// }
-
-// var num = numbers.getElementsByTagName('span')
-
-// var index = 0;
-// const nextNum = () => {
-//     num[index].style.display = 'none'   
-//     index =(index + 1) % num.length;
-//     num[index].style.display = 'initial'   
-// }
-// const prevNum = () => {
-//     num[index].style.display = 'none'   
-//     index =(index - 1 + num.length)  % num.length;
-//     num[index].style.display = 'initial'   
-// }
-
 
 let next = document.querySelector(".next")
 let prev = document.querySelector(".prev")
@@ -41,3 +21,57 @@ prev.addEventListener("click", (e) => {
 })
 
 
+
+// let next = document.querySelector(".next")
+// let prev = document.querySelector(".prev")
+
+
+// for (let i = 0; i < 100 ; i++){
+//     let span = document.createElement("span")
+//     span.textContent = i
+//     numbers.appendChild(span)
+// }
+
+
+// let span = numbers.querySelectorAll('span')
+// let x = 0
+
+// next.addEventListener("click", (e) => {
+
+//     span[x].style.display = "none"
+//     x = (x + 1) % span.length
+//     span[x].style.display = "initial"
+// })
+// prev.addEventListener("click", (e) => {
+
+//     span[x].style.display = "none"
+//     x = (x - 1 + span.length)  % span.length
+//     span[x].style.display = "initial"
+// })
+
+
+
+
+
+
+
+
+// for(let i = 0; i< 100 ; i++){
+//     let span = document.createElement("span")  
+//     span.textContent = i;
+//     numbers.appendChild(span)
+// }
+
+// var num = numbers.getElementsByTagName('span')
+
+// var index = 0;
+// const nextNum = () => {
+//     num[index].style.display = 'none'   
+//     index =(index + 1) % num.length;
+//     num[index].style.display = 'initial'   
+// }
+// const prevNum = () => {
+//     num[index].style.display = 'none'   
+//     index =(index - 1 + num.length)  % num.length;
+//     num[index].style.display = 'initial'   
+// }
